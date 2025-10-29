1d8a17b5e0c13ea2a77baabba247bcc1a36b7d1b
Siroj Wongpitakroj <swongpitakroj24@gmail.com>
2025-10-29 21:24:58 +0700

    Complete Responsive Product Page

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
+//Hamburger DropDown Animation
 $("#hamburger-menu").click(function() {
     $("#dropdown-menu").toggle();
 });
 
+//Product Cards link to Product Page
+$(".cards").click(function() {
+    window.location.href = "./product.html";
+});
+
+//Slicing Long Text
+var longText = $(".long-text").text();
+var screenWidth = $(window).width();
+if (longText.length > 150 && screenWidth < 800) {
+    $(".long-text").text(longText.slice(0,150) + " . . . ");
+}
